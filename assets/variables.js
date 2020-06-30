a60c4f21ef1e7e4ae054a47f0e106dcd33babd6d
Gilyaa <ragielbyan@gmail.com>
2020-06-30 18:46:35 +0700

    Update variables.js

--- assets/variables.js
+++ assets/variables.js
@@ -1,5 +1,5 @@
 export default {
-  nicknames: ["Ava", "Babe", "Sayang", "Avakuu", "Cintuu"],
+  nicknames: ["Raniku sayang mamih muda istriku tercintaa],
   greetings: {
     evening: "Good Evening",
     afternoon: "Good Afternoon",
